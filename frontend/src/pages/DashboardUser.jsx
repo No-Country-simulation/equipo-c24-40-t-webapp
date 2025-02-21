c01568f425804611badacd17516e3ffb6eb23477
mplatab <marcosplata98@gmail.com>
2025-02-21 00:01:12 -0500

    Updated frontend files

--- frontend/src/pages/DashboardUser.jsx
+++
@@ -1,9 +0,0 @@
-export default function DashboardUser() {
-    return (
-      <div className="p-10">
-        <h2 className="text-2xl font-bold">Historial de Reservas</h2>
-        {/* Aquí se listarían las reservas del usuario */}
-      </div>
-    );
-  }
-  
